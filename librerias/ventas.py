06dacf5caf00e74aca0e8e1e3a69239073ee30f0
JaumeG13 <jaumegenaro02@gmail.com>
2026-02-21 23:21:49 +0100

    Update ventas.py

--- librerias/ventas.py
+++ librerias/ventas.py
@@ -6,6 +6,7 @@ RUTA_CSV="datos/productos.csv"
 SEPARADOR=";"
 
 df_menu=pd.read_csv(RUTA_CSV, sep=SEPARADOR, decimal=",")
+df_menu.set_index("id", inplace=True)
 
 print(df_menu)      #imprime el menu segun el csv
 pedido=[]           #inicializa una lista para guardar los id del pedido
@@ -15,16 +16,27 @@ while True:
     producto=int(input("Pide un producto: (-1 para salir) "))
     if producto == -1:  #si el pedido es -1 cierra el bucle, antes de añadirlo a la lista
         break
-    pedido.append(producto)
+    
+    if producto in df_menu.index:  #si el producto existe en el menu lo añade a la lista, si no, muestra un mensaje de error
+        pedido.append(producto)
+    else:
+        print("Producto no disponible")
 
 #1.3 Procesado de la venta
-subtotal=df_menu.loc[pedido, "Precio"].sum()
+productos_seleccionados=df_menu.loc[pedido]
+
+subtotal=productos_seleccionados["Precio"].sum()
 iva=subtotal*0.10
 total=subtotal+iva
 
 ticket = {
-    "productos": "nombre",
+    "productos": productos_seleccionados.reset_index().to_dict(orient="records"),
     "subtotal": round(subtotal, 2),
     "iva": round(iva, 2),
     "total": round(total, 2)
 }
+
+with open("ticket.json", "w") as f:
+    json.dump(ticket, f, indent=4)
+
+print("Ticket guardado correctamente.")
