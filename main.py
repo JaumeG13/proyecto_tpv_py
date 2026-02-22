f9d18ef56cf148dbf90e48caf4b41d7f16d71723
JaumeG13 <jaumegenaro02@gmail.com>
2026-02-22 21:22:33 +0100

    integrado parte 1 con 2.2

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from librerias.ventas import hacerPedido, procesarVenta, guardarTicket, registrarLog
+from librerias.panel import accesoPanel
 
 
 def main():
-    pedido = hacerPedido()
+    while True:
+        opcion = int(input("Seleccione una opción:\n1. Hacer un pedido\n2. Acceder al panel de control\n3. Salir\n"))
+        match opcion:
+            case 1:
+                pedido = hacerPedido()
 
-    if len(pedido) == 0:
-        print("No se ha realizado ningun pedido.")
-        return
+                if len(pedido) == 0:
+                    print("No se ha realizado ningun pedido.")
+                    return
 
-    ticket = procesarVenta(pedido)
+                ticket = procesarVenta(pedido)
 
-    guardarTicket(ticket)
-    registrarLog(ticket["total"])
+                guardarTicket(ticket)
+                registrarLog(ticket["total"])
 
-    print("Venta realizada correctamente.")
-    print("Total a pagar:", ticket["total"], "€")
-
+                print("Venta realizada correctamente.")
+                print("Total a pagar:", ticket["total"], "€")
+            case 2:
+                accesoPanel()
+            case 3:
+                print("Saliendo del programa.")
+                return
+            case _: 
+                print("Error: Introduzca una opción correcta.")
 
 if __name__ == "__main__":
     main()
