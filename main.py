421cc832533bcd74d0cb9e8543cc242230e56cd6
JaumeG13 <jaumegenaro02@gmail.com>
2026-02-22 23:08:48 +0100

    arreglar error en opcion 2
    cuando se seleccionaba la opcion de acceder al panel de control, aunque introduzcas la contraseña incorrecta 3 veces, se ejecutaba la funcion de estadisticas que solo debe ejecutarse cuando se inicia sesion

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from librerias.ventas import hacerPedido, procesarVenta, guardarTicket, registrarLog
-from librerias.panel import accesoPanel
+from librerias.panel import accesoPanel, estadisticas, nombre_pizzeria
+
 
 
 def main():
     while True:
+        print(f"------- TPV {nombre_pizzeria} -------")
         opcion = int(input("Seleccione una opción:\n1. Hacer un pedido\n2. Acceder al panel de control\n3. Salir\n"))
         match opcion:
             case 1:
@@ -20,11 +22,15 @@ def main():
 
                 print("Venta realizada correctamente.")
                 print("Total a pagar:", ticket["total"], "€")
-            case 2:
-                accesoPanel()
+
+            case 2: #Si el acceso al panel es correcto, muestra las estadisticas
+                if accesoPanel():
+                    estadisticas()
+
             case 3:
-                print("Saliendo del programa.")
+                print("Saliendo ...")
                 return
+            
             case _: 
                 print("Error: Introduzca una opción correcta.")
 
